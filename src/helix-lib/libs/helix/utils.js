7c1530c6cfb90564561822784a3b8a341a59f604
sachushaji <sachushajiabraham@gmail.com>
2019-06-14 13:48:04 +0530

    Changinging helix constants

--- src/helix-lib/libs/helix/utils.js
+++ src/helix-lib/libs/helix/utils.js
@@ -17,57 +17,58 @@ import { NODELIST_URL, MAX_REQUEST_TIMEOUT } from '../../config';
 import Errors from '../errors';
 import { roundDown } from '../utils';
 
-export const MAX_SEED_LENGTH = 81;
+export const MAX_SEED_LENGTH = 64;
 
-export const MAX_SEED_TRITS = MAX_SEED_LENGTH * 3;
+export const MAX_SEED_BITS = MAX_SEED_LENGTH * 4;
 
-export const SEED_CHECKSUM_LENGTH = 3;
+export const SEED_CHECKSUM_LENGTH = 8;
 
 export const ADDRESS_LENGTH_WITHOUT_CHECKSUM = MAX_SEED_LENGTH;
 
-export const ADDRESS_LENGTH = 90;
+export const ADDRESS_LENGTH = 72;
 
 export const CHECKSUM_LENGTH = ADDRESS_LENGTH - ADDRESS_LENGTH_WITHOUT_CHECKSUM;
 
-export const VALID_SEED_REGEX = /^[A-Z9]+$/;
+export const VALID_SEED_REGEX = /^[a-f0-9]+$/;
 
 export const VALID_ADDRESS_WITHOUT_CHECKSUM_REGEX = VALID_SEED_REGEX;
 
-export const VALID_ADDRESS_WITH_CHECKSUM_REGEX = /^[A-Z9]{90}$/;
+export const VALID_ADDRESS_WITH_CHECKSUM_REGEX = /^[a-f0-9]{72}$/;
 
-export const TOTAL_IOTA_SUPPLY = 2779530283277761;
+export const TOTAL_HELIX_SUPPLY = 2779530283277761;
 
-export const HASH_SIZE = 81;
+export const HASH_SIZE = 64;
 
-export const TRANSACTION_TRYTES_SIZE = 2673;
+export const TRANSACTION_BYTES_SIZE = 1536;
 
-export const EMPTY_HASH_TRYTES = '9'.repeat(HASH_SIZE);
+export const EMPTY_HASH_BYTES = '0'.repeat(HASH_SIZE);
 
-export const EMPTY_TRANSACTION_TRYTES = '9'.repeat(TRANSACTION_TRYTES_SIZE);
+export const EMPTY_TRANSACTION_BYTES = '0'.repeat(TRANSACTION_BYTES_SIZE);
 
 export const EMPTY_TRANSACTION_MESSAGE = 'Empty';
 
-export const IOTA_DENOMINATIONS = ['i', 'Ki', 'Mi', 'Gi', 'Ti'];
+export const HELIX_DENOMINATIONS = ['h', 'Kh', 'Mh', 'Gh', 'Th'];
 
-/**
- * Converts trytes to bytes
- *
- * @method convertFromTrytes
- * @param {string} trytes
- *
- * @returns {string}
- */
-export const convertFromTrytes = (trytes) => {
-    const trytesWithoutNines = trytes.replace(/9+$/, '');
-    const message = iota.utils.fromTrytes(trytesWithoutNines);
+// Since the helix already based on bytes, this may not be needed
+// /**
+//  * Converts trytes to bytes
+//  *
+//  * @method convertFromTrytes
+//  * @param {string} trytes
+//  *
+//  * @returns {string}
+//  */
+// export const convertFromTrytes = (trytes) => {
+//     const trytesWithoutNines = trytes.replace(/9+$/, '');
+//     const message = iota.utils.fromTrytes(trytesWithoutNines);
 
-    /* eslint-disable no-control-regex */
-    if (trytesWithoutNines && message && /^[\x00-\xFF]*$/.test(message)) {
-        return message;
-    }
-    /* eslint-enable no-control-regex */
-    return EMPTY_TRANSACTION_MESSAGE;
-};
+//     /* eslint-disable no-control-regex */
+//     if (trytesWithoutNines && message && /^[\x00-\xFF]*$/.test(message)) {
+//         return message;
+//     }
+//     /* eslint-enable no-control-regex */
+//     return EMPTY_TRANSACTION_MESSAGE;
+// };
 
 /**
  * Gets checksum.
