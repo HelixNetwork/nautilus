eeee75552e42e245a948358371d92cd3a7b14aee
sachushaji <sachushajiabraham@gmail.com>
2019-06-19 17:13:33 +0530

    Fixing Bug in ChangeNode

--- src/helix-lib/libs/helix/index.js
+++ src/helix-lib/libs/helix/index.js
@@ -6,14 +6,13 @@ import Quorum from './quorum';
 import { DEFAULT_NODE, DEFAULT_NODES, DEFAULT_NODE_REQUEST_TIMEOUT, QUORUM_SIZE } from '../../config';
 
 /** Globally defined HELIX instance */
-const helixAPI =  composeAPI({
+let helixAPI =  composeAPI({
     provider:DEFAULT_NODE.url
 });
 
 // Set node request timeout
 // TODO
 // helixAPI.api.setApiTimeout(DEFAULT_NODE_REQUEST_TIMEOUT);
-IOTA
 /** Globally defined Quorum instance */
 export const quorum = new Quorum({
     nodes: DEFAULT_NODES,
