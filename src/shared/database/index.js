f550aeb938f0e050a825f3bcea7ec0443a4becc6
mahi <mahesh30795@gmail.com>
2019-07-12 14:41:07 +0530

    seed import

--- src/shared/database/index.js
+++ src/shared/database/index.js
@@ -308,14 +308,13 @@ class Wallet {
 
     static get latestSettings() {
         const dataForCurrentVersion = Wallet.getObjectForId();
-        console.log(dataForCurrentVersion);
         return dataForCurrentVersion.settings;
     }
 
 
     /**
      * Wallet data (as plain object) for most recent version.
-     */latestSettings
+     */
     static get latestDataAsPlainObject() {
         const data = Wallet.latestData;
 
