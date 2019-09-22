99586bd87bd999b991d7e9e602fbeda531c2979d
sachu shaji abraham <sachushajiabraham@gmail.com>
2019-09-22 16:17:02 +0530

    Removing un wanted codes from menu

--- src/desktop/native/menu.js
+++ src/desktop/native/menu.js
@@ -12,7 +12,7 @@ const state = {
   authorised: false,
   enabled: true
 };
-//const GTOKEN="";
+
 let language = {
   devTool: "Toggle Developer Tools",
   reLoad:"Reload Wallet",
@@ -189,7 +189,6 @@ export const initMenu = (app, getWindowFunc) => {
           {
             label: `${language.checkUpdate}...`,
             click: () => {
-              //autoUpdater.setFeedURL({ provider: 'github', owner: "netobjex", repo: "wallet",private:true, token:GTOKEN })
               autoUpdater.checkForUpdates();
             },
             enabled: state.enabled
@@ -478,7 +477,6 @@ export const initMenu = (app, getWindowFunc) => {
     });
 
     ipcMain.on("updates.check", () => {
-     // autoUpdater.setFeedURL({ provider: 'github', owner: "netobjex", repo: "wallet",private:true, token:GTOKEN });
       autoUpdater.checkForUpdates();
     });
 
