2c9ea1b90fec056f08c97f3edf5df27abaf2f8f7
trycatchajith <hello@trycatchajith.com>
2019-10-11 11:12:15 +0530

    Test case initial commit

---
+++ src/desktop/__tests__/e2e/Onboarding.test.js
@@ -0,0 +1,27 @@
+console.log("Entered here");
+
+describe('Nautilus desktop end-to-end', () => {
+    test('Nautilus onboarding existing seed', async () => {
+        const page = await global.__getBrowserPage('onboarding', false);
+
+        // Language selection
+        await page.waitForSelector('#to-seed-intro');
+        await page.click('#to-seed-intro');
+
+        page.close();
+
+        expect(selector).toBeTruthy();
+    }, 15000);
+
+    test('Nautilus onboarding new seed', async () => {
+        const page = await global.__getBrowserPage('onboarding', false);
+
+        // Language selection
+        await page.waitForSelector('#to-seed-intro');
+        await page.click('#to-seed-intro');
+
+        page.close();
+
+        expect(selector).toBeTruthy();
+    }, 150000);
+});
