46435d19eab1d9b6446dcd197effc915d05ca2f7
remyajoseph <remyavellankal@gmail.com>
2019-11-27 09:58:01 +0530

    checksum

--- src/desktop/__tests__/ui/components/checksum.test.jsx
+++ src/desktop/__tests__/ui/components/checksum.test.jsx
@@ -7,14 +7,14 @@ const props = {
     address: 'a28c44524d557d2276170f1e662a9a3abccebb7cc6559a0c00b628613793a710125dc65c',
 };
 
-describe('Checksum component', () => {
-    test('Render the component', () => {
+describe('test for Checksum component', () => {
+    test('Render the Checksum component', () => {
         const wrapper = shallow(<Checksum {...props} />);
 
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('Address value', () => {
+    test('Test with valid address value', () => {
         const wrapper = shallow(<Checksum {...props} />);
 
         expect(wrapper.text()).toEqual(
@@ -22,7 +22,7 @@ describe('Checksum component', () => {
         );
     });
 
-    test('Checksum highlight value', () => {
+    test('Test for Checksum highlight value', () => {
         const wrapper = shallow(<Checksum {...props} />);
         expect(wrapper.find('mark').text()).toEqual('125dc65c');
     });
