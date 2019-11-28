f2da1f44ab75383608d24870ed495d9f10ac2de4
remyajoseph <remyavellankal@gmail.com>
2019-11-28 13:29:06 +0530

    loading

--- src/desktop/__tests__/ui/components/loading.test.jsx
+++ src/desktop/__tests__/ui/components/loading.test.jsx
@@ -11,20 +11,20 @@ const props = {
     onEnd : jest.fn(),
 };
 
-describe('Loading component', () => {
-    test('Render the component', () => {
+describe('Test for Loading component', () => {
+    test('Render the Loading component', () => {
         const wrapper = shallow(<Loading {...props} />);
 
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('inline true state', () => {
+    test('Checks the inline true state', () => {
         const wrapper = shallow(<Loading {...props} />);
         
         expect(wrapper.hasClass('inline')).toBeTruthy();
     });
 
-    test('inline false state', () => {
+    test('Checks the inline false state', () => {
         const mockProps = Object.assign({}, props, { inline: false });
         const wrapper = shallow(<Loading {...mockProps} />);
 
