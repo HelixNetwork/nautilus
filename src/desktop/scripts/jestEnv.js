c14abcd6467a76863df5a9c97ed6dfe7a884b870
mahi <mahesh30795@gmail.com>
2019-12-20 19:05:34 +0530

    removed console from jestEnv

--- src/desktop/scripts/jestEnv.js
+++ src/desktop/scripts/jestEnv.js
@@ -31,7 +31,6 @@ class PuppeteerEnvironment extends ElectronEnvironment {
         this.browser = null;
 
         const getNewPage = async (route, isAuthorised) => {
-            console.log('isAuth', isAuthorised);
             if (!this.browser) {
                 this.browser = await puppeteer.connect({
                     browserWSEndpoint: wsEndpoint,
