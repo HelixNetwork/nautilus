c85d372702e95ead6cc85222e99ae7715de1d53c
sachu shaji abraham <sachushajiabraham@gmail.com>
2019-12-29 19:49:29 +0530

    Fixed welcome test

--- src/desktop/__tests__/ui/views/onboarding/welcome.test.jsx
+++ src/desktop/__tests__/ui/views/onboarding/welcome.test.jsx
@@ -5,12 +5,12 @@ describe('Onboarding welcome view', () => {
     });
 
     test('Render view', async () => {
-        const snapshot = await global.__screenshot('/onboarding', false);
+        const snapshot = await global.__screenshot('onboarding', false,10000);
         expect(snapshot).toMatchImageSnapshot({
             customSnapshotsDir: `${__dirname}/__snapshots__/`,
             failureThreshold: '0.05',
             failureThresholdType: 'percent',
             customSnapshotIdentifier: 'welcome.test.jsx',
         });
-    }, 10000);
+    }, 150000);
 });
