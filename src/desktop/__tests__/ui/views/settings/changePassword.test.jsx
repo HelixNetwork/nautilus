e5af7d57924fc132fa53c251fcb07d8e3cfa7b5a
sachu shaji abraham <sachushajiabraham@gmail.com>
2020-01-06 18:32:38 +0530

    Fixing change password test

--- src/desktop/__tests__/ui/views/settings/changePassword.test.jsx
+++ src/desktop/__tests__/ui/views/settings/changePassword.test.jsx
@@ -1,6 +1,6 @@
 describe('Settings change password view', () => {
     test('Render view', async () => {
-        const snapshot = await global.__screenshot('settings/advanced', true,600);
+        const snapshot = await global.__screenshot('settings/password', true);
 
         expect(snapshot).toMatchImageSnapshot({
             customSnapshotsDir: `${__dirname}/__snapshots__/`,
