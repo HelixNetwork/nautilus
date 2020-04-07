36d0fff34a1a3b4e6d22ebfefeb6c999b6bb5c5f
sachu shaji abraham <sachushajiabraham@gmail.com>
2020-04-07 15:12:57 +0530

    mAking delay in timestamp

--- src/desktop/src/ui/components/list.js
+++ src/desktop/src/ui/components/list.js
@@ -328,7 +328,7 @@ export class ListComponent extends React.PureComponent {
                             return (
                                 <div className={css.lists} key={key} onClick={() => setItem(transaction.bundle)}>
                                     {isConfirmed ? (
-                                        Date.now() - transaction.timestamp < 60 ? (
+                                        Date.now() / 1000 - transaction.timestamp < 70 ? (
                                             <div className={css.column_pending}>
                                                 <div className={css.column_cnt}>
                                                     <h4 className={css.sent_heading}>PENDING</h4>
